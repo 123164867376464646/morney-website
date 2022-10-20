89512777ad099b8c9e55cc29b56a8bd77927494a
linmumu <1054742101@qq.com>
2022-10-21 03:13:02 +0800

    deploy

---
+++ precache-manifest.157d7fe07ac3ffffb67453533c922dcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a358f837914976235567",
+    "url": "/morney-website/css/app.a6c5b695.css"
+  },
+  {
+    "revision": "bc266dd7d6b8bfcb46250759d10fa190",
+    "url": "/morney-website/img/qrcode.bc266dd7.png"
+  },
+  {
+    "revision": "2d48621daac7a869784d19a6569a90bb",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "a358f837914976235567",
+    "url": "/morney-website/js/app.303b3d99.js"
+  },
+  {
+    "revision": "438839ccfa21f4337d58",
+    "url": "/morney-website/js/chunk-vendors.327e1458.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
